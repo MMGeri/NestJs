6e74f728c7e4d82863b1bd146e9837412687b91b
Márta Gergely Márk <h049248@stud.u-szeged.hu>
2022-06-04 22:05:27 +0200

    Controllers

---
+++ src/controllers/profile/profile.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get } from '@nestjs/common';
+
+import {  Request, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/strategies/jwt-auth.guard';
+import { ApiBearerAuth, ApiHeader } from '@nestjs/swagger';
+
+@Controller('profile')
+export class ProfileController {
+
+    @Get()
+    @ApiBearerAuth()
+    @UseGuards(JwtAuthGuard)
+    async getProfile(@Request() req) {
+        return req.user;
+    }
+    
+}
